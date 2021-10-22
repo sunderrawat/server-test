e24611d9f7840e739c97755bd8512b391617e2b4
Sunder Rawat <sunderrawat777@gmail.com>
2021-10-22 16:19:07 +0530

    change server to 4000

--- app.js
+++ app.js
@@ -8,6 +8,6 @@ app.use(express.static("public"));
 //   res.send("hello world");
 // });
 
-app.listen(4002, () => {
-  console.log("app is listing on port 4002");
+app.listen(4000, () => {
+  console.log("app is listing on port 4000");
 });
